1b2245329ec5c148d6f9da2c8e5a1bfe44f823c9
J <98541904+DownMan01@users.noreply.github.com>
2025-03-10 00:23:17 +0800

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -145,6 +145,7 @@ export default async function AirdropPage({ params }: { params: { id: string } }
         {airdrop.description && (
           <div className="mb-12 p-6 bg-secondary/50 rounded-lg text-secondary-foreground">
             <p className="leading-relaxed">{airdrop.description}</p>
+             <div className="h-px bg-secondary w-full mb-4"></div>
           </div>
         )}
 
